7a7e2bcf8025ea48a30a476d2d477a0a5d87cb79
anselm <anselm@gmail.com>
2025-10-06 00:49:42 -0700

    added image test

--- seed-data/root-group.info.js
+++ seed-data/root-group.info.js
@@ -9,6 +9,7 @@ export default {
   createdAt: new Date().toISOString(),
   updatedAt: new Date().toISOString(),
   permissions: ['public:view'],
+  depiction: "https://www.doc.govt.nz/thumbs/hero/globalassets/images/places/fiordland/kepler-track/kepler-1920.jpg",
   metadata: {
     isRoot: true,
     description: 'This is the root group that serves as the top-level container for all other entities'
